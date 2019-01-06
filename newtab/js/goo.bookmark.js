618e8f411dfbbc796a4865e2356fa3d3c287a116
Davide 'Folletto' Casali <folletto.id@gmail.com>
2019-01-06 19:08:52 +0000

    Alert for chrome:// bookmarks

--- newtab/js/goo.bookmark.js
+++ newtab/js/goo.bookmark.js
@@ -6,11 +6,21 @@ export class Bookmark {
   }
 
   render() {
+    let extra = '';
+    let url = new URL(this.bookmark.url);
+    if (url.protocol === 'chrome:') {
+      extra = goo.onClick(this.onClickChrome.bind(this));
+    }
+
     return `
-      <a href="${this.bookmark.url}">
+      <a href="${this.bookmark.url}" ${extra}>
         <span class="bookmark__img"><img src="chrome://favicon/size/16@2x/${this.bookmark.url}" /></span>
         <span class="bookmark__label">${this.bookmark.title}</span>
       </a>
     `;
+  }
+
+  onClickChrome() {
+    alert('\nDue to Chrome security limitations, chrome:// urls are currently unsupported.\n\nSorry!');
   }
 }
